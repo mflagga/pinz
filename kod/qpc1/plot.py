ef2f2ea23762c88ebd7448a77cff32385f138375
Maciej <maciejjjj4@gmail.com>
2026-05-31 16:47:05 +0200

    latex plot style

--- kod/qpc1/plot.py
+++ kod/qpc1/plot.py
@@ -1,6 +1,7 @@
 # pyright: reportUndefinedVariable=false
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams.update({"text.usetex":True, "font.size": 16})
 
 def wczytaj(*nazwy): # funkcja do wczytywania plikow od clauda
     for nazwa in nazwy:
@@ -9,11 +10,14 @@ def wczytaj(*nazwy): # funkcja do wczytywania plikow od clauda
 # wczytanie plików
 wczytaj("dyspfile","kxfile")
 
-size = 5.5
+size = 8
 
 plt.figure(figsize=(1.2*size,size))
 for j in range(len(dyspfile[0])):
     plt.plot(kxfile,dyspfile[:,j],c="tab:blue")
+plt.grid(ls=":")
+plt.xlabel(rf"$k_x\ \left[\frac{{1}}{{nm}}\right]$")
+plt.ylabel(rf"$E\ \left[eV\right]$")
 plt.tight_layout()
 plt.savefig("dyspfile.png",dpi=150)
 plt.close()
